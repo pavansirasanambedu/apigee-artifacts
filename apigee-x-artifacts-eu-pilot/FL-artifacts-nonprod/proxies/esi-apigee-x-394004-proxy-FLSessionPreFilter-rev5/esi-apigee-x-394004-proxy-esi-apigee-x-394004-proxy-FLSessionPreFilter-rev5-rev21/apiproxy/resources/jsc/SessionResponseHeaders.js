2c7ea12f6af02592b4691964a183534742d51764
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-16 11:35:37 +0530

    Delete apigee-x-artifacts-eu-pilot directory

--- apigee-x-artifacts-eu-pilot/FL-artifacts-nonprod/proxies/esi-apigee-x-394004-proxy-FLSessionPreFilter-rev5/esi-apigee-x-394004-proxy-esi-apigee-x-394004-proxy-FLSessionPreFilter-rev5-rev21/apiproxy/resources/jsc/SessionResponseHeaders.js
+++
@@ -1,3 +0,0 @@
-var sessionId = context.getVariable("session.id");
-context.setVariable("request.header.jwt-token", "lkjhgsdfghrtyu");
-context.setVariable("request.header.isAuthenticated", "true");
--- apigee-x-artifacts-eu-pilot/FL-artifacts-nonprod/proxies/message-github-proxy_rev2_2023_09_04/esi-apigee-x-394004-proxy-message-github-proxy_rev2_2023_09_04-rev2/apiproxy/resources/jsc/randomnumber.js
+++
@@ -1,2 +0,0 @@
-id = context.setVariable("id","Math.floor(Math.random() * (max - min + 1)) + min")
-
