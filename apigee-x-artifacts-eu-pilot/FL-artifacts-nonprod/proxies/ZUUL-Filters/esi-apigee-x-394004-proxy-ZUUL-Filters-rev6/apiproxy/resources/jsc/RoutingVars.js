e17eaa03a81b199f107f70e0a9c0641355b08854
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-18 10:44:53 +0530

    Delete apigee-x-artifacts-eu-pilot/FL-artifacts-nonprod directory

--- apigee-x-artifacts-eu-pilot/FL-artifacts-nonprod/proxies/ZUUL-Filters/esi-apigee-x-394004-proxy-ZUUL-Filters-rev6/apiproxy/resources/jsc/RoutingVars.js
+++
@@ -1,3 +0,0 @@
-// FL implemented -- Domain name, hostname, session path, siteId, region, cachedservices, 
-sessionId = content.getVariable("session.id");
-registerId = context.getVariable("register.id");
