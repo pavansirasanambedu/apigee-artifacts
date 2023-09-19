40a7a9c1365e1445c665649db0e6626b45a291fc
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-19 22:31:09 +0530

    Delete apigee-x-artifacts-eu-pilot directory

--- apigee-x-artifacts-eu-pilot/FL-artifacts-nonprod/proxies/sessionid-cache/esi-apigee-x-394004-proxy-sessionid-cache-rev6/apiproxy/resources/jsc/jsonpayload.js
+++
@@ -1,4 +0,0 @@
-var responseJson = JSON.parse(context.getVariable("response.content"));
-var cachekey = context.getVariable("session.id");
-responseJson.cachekey = cachekey;
-context.setVariable("response.content",JSON.stringify(responseJson));
