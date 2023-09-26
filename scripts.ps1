7f1bcf17ec0c10715bce811ee9d3cf08cde71190
pavansirasanambedu <144771691+pavansirasanambedu@users.noreply.github.com>
2023-09-26 13:06:42 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -32,6 +32,39 @@ Write-Host "Exited out of the IF with: $org"
 $path = $baseURL
 Invoke-RestMethod -Uri "https://apigee.googleapis.com/v1/organizations/$org/apis" -Method 'GET' -Headers $headers -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60 -OutFile "test.json"
 
+if(!(test-path -PathType container apigee)){
+      mkdir "apigee"
+      cd apigee
+      Write-Host "inside if"
+}
+else {
+      cd apigee
+      Write-Host "else"
+}
+
+if(!(test-path -PathType container apps)){
+      mkdir "apps"
+      cd apps
+      Write-Host "inside if"
+}
+else {
+      cd apps
+      Write-Host "else"
+}
+# PowerShell script
+$github_actor = "${{ github.actor }}"  # Use the GitHub actor variable
+
+# Remove parentheses if they exist
+$github_actor = $github_actor -replace '\s*\([^)]*\)\s*', ''
+
+# Set the modified value as an output variable
+Write-Output "##vso[task.setvariable variable=githubActor;isOutput=true]$github_actor"
+
+
+
+
+
+
 
 
 # # --------------------Apigee All Artifacts-------------------------------------------
